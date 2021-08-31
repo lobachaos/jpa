24dbb89dca232d8cb11fcebf2f0d33b3731e202a
lobachaos <zwolfsasa@gmail.com>
2021-08-31 11:19:10 -0400

    First Commit

---
+++ jpa/src/main/java/modelo/basico/ObterUsuario.java
@@ -0,0 +1,22 @@
+package modelo.basico;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class ObterUsuario {
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Usuario usuario = em.find(Usuario.class, 1);
+		System.out.println(usuario.getNome());
+		
+		em.close();
+		emf.close();
+		
+		
+	}
+
+}
---
+++ jpa/src/main/java/modelo/basico/ObterUsuarios.java
@@ -0,0 +1,32 @@
+package modelo.basico;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+public class ObterUsuarios {
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		String jpql= "select u from Usuario u";
+		TypedQuery<Usuario> query = em.createQuery(jpql, Usuario.class);
+		query.setMaxResults(3);
+		
+		List<Usuario> usuarios = query.getResultList();
+		
+		for(Usuario usuario: usuarios) {
+			System.out.println("ID : " + usuario.getId() + " Nome : " + usuario.getNome());
+		}
+		
+		
+		
+		em.close();
+		emf.close();
+	}
+
+}
